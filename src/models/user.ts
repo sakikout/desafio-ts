ad7973cac8bfac701772b3a5cec3da875b548e02
sakikout <bedalfior@gmail.com>
2025-06-19 15:05:36 -0300

    Create User class

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
 // Criando uma interface para o usuário
-export interface User {
+export interface UserInterface {
   id : string;
   name: string;
   email: string;
@@ -12,46 +12,83 @@ export interface User {
 
 }
 
+export class User implements UserInterface {
+  id: string;
+  name: string;
+  email: string;
+  age: number;
+
+  constructor(name: string, email: string, age: number) {
+    this.id = uuidv4();
+    this.name = name;
+    this.email = email;
+    this.age = age;
+  }
+
+  // Método para exibir informações do usuário
+  showUserInfo() {
+    console.log(`Usuário:
+      ID: ${this.id}
+      Nome: ${this.name}
+      Email: ${this.email}
+      Idade: ${this.age}`);
+  }
+}
+
 // Array para armazenar os usuários criados
 const users : Array<User> = [];
 
-// Função para criar um novo usuário
-// Recebe um objeto com as propriedades do usuário e retorna um novo usuário
-export function createUser(user : { name: string; email: string; age: number; }) {
-  const newUser : User = {
-    id: uuidv4(),
-    name: user.name,
-    email: user.email,
-    age: user.age,
-    showUserInfo: () => {
-      console.log(`Usuário:
-        ID: ${newUser.id}
-        Nome: ${newUser.name}
-        Email: ${newUser.email}
-        Idade: ${newUser.age}`);
-    }
-  };
-
-  users.push(newUser);
-
-  return newUser;
+// Função para adicionar um novo usuário ao array de usuários
+export function addUser(user : User) {
+  users.push(user);
+  console.log(`Usuário ${user.name} adicionado com sucesso ao banco de usuários!`);
+  return user;
 }
 
 // Função para atualizar um usuário no array de usuários
 export function updateUser(user : User) {
   const index = users.findIndex((item) => item.id === user.id);
+  if (index === -1) {
+    console.log('Usuário não encontrado');
+    return -1;
+  }
 
   users[index] = user;
+  return index;
+}
+
+// Função para buscar um usuário dado seu email
+export function getUserByID(id : string) {
+  const user = users.find((item) => item.id === id);
+
+  if (!user) {
+    console.log('Usuário não encontrado');
+    return null;
+  }
+
+  return user;
 }
 
 // Função para buscar um usuário dado seu email
 export function getUserByEmail(email : string) {
-  return users.find((item) => item.email === email);
+  const user = users.find((item) => item.email === email);
+
+  if (!user) {
+    console.log('Usuário não encontrado');
+    return null;
+  }
+
+  return user;
 }
 
 // Função para remover um usuário dado seu ID
 export function removeUser(id : string) {
   const index = users.findIndex((item) => item.id === id);
 
-  users.splice(index, 1);
+  if (index === -1) {
+    console.log('Usuário não encontrado');
+    return null;
+  }
+
+  return users.splice(index, 1);
 }
